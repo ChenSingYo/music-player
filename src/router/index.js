07df78dcfcbfc46d00913772d852d97aaf7a3445
ChenSingYo <chensingyo@gmail.com>
2022-10-01 22:15:27 +0800

    add Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
-import { createRouter, createWebHistory } from "vue-router";
+import useUserStore from "@/stores/user";
 import Home from "@/views/Home.vue";
-import About from "@/views/About.vue"
+import { createRouter, createWebHashHistory } from "vue-router";
+
 const routes = [
   {
     path: "/",
@@ -15,10 +16,48 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import("../views/About.vue"),
   },
+  {
+    name: "manage",
+    path: "/manage-music",
+    // optional route name
+    alias: "/manage",
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import("../views/Manage.vue"),
+    beforeEnter(to, from, next) {
+      next();
+    },
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: {
+      name: "home",
+    },
+  },
 ];
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: "text-yellow-500",
+});
+
+router.beforeEach((to, from, next) => {
+  // console.log(to.meta)
+  if (!to.meta.requiresAuth) {
+    next();
+    return;
+  }
+  const store = useUserStore();
+
+  if (store.userLoggedIn) {
+    next();
+  } else {
+    next({ name: "home" });
+  }
 });
 
 export default router;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+    <h1>this is about page</h1>
+</template>
 
 <script>
 export default {
